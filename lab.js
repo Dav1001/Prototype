50004224ec861e2f03c6762b43815203fb4ac348
Dav1001 <studenta@aitc.eif.local>
2018-05-19 13:19:43 +0400

    Prototype

---
+++ lab.js
@@ -0,0 +1,68 @@
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ orinak 1
+// // constructor
+// function User(name, age){
+// 	this.name = name;
+// 	this.age = age;
+// 	this.isAdmin = false;
+// 	this.foo = function(){
+// 		console.log(this.name + this.age);
+// 		// return true
+// 	}
+// }
+// var user1 = new User("Davit", 19);
+// user1.foo2 = function(){};
+
+
+// var user2 = new User('Hayk', 20);
+
+
+
+// console.log(user1.name);
+// console.log(user1.age);
+// console.log(user1.constructor);
+
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\   orinak 2
+// var o1 = {};
+// var o2 = new Object();
+
+// var arr1 = [];
+// var arr2 = new Array();
+
+// if(o1 === o2) console.log('true');
+
+// console.log(typeof o1);
+// console.log(typeof o2);
+
+
+// function User(name, age){
+// 	this.name = name;
+// 	this.age = age;
+// 	this.isAdmin = false;
+// 	this.say = function (w){console.log(w);}
+// 	}
+
+// User.prototype.x = 5;
+
+// var u1 = new User('Davit', 19);	
+// var u2 = new User('Ashot', 20);
+
+// u1.say('hello');
+// u2.say('hello');	
+
+
+
+
+function User(name, age){
+	this.name = name;
+	this.age = age;
+	this.foo = function (){console.log('FOO');}
+}
+// User.prototype.foo = 5;
+User.prototype.x = 5;
+
+user1 = new User('Davit', 19);
+
+for (var item in user1) {
+	if(user1.hasOwnProperty(item))
+		console.log(item);
+}
